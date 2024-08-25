869c2a8c19325da3006caf640307c821999a3f53
Ahmed Abouali <ahmedabouali91@gmail.com>
2024-08-25 12:44:23 +0200

    Update MarkerManager.cpp

--- src/MarkerManager.cpp
+++ src/MarkerManager.cpp
@@ -1,42 +1,49 @@
 #include "MarkerManager.h"
+#include "MarkersParser.h"
 
 #include <sstream>
-
+#include <memory>  // For std::unique_ptr
+#include <stdexcept>  // For std::out_of_range exception
 
 MarkerManager::MarkerManager()
 {
-	parser = new MarkersParser();
-	markers = parser->parse();
+    parser = std::make_unique<MarkersParser>();
+    markers = parser->parse();
 }
 
-
-string MarkerManager::getMarker(int i)
+std::string MarkerManager::getMarker(int i)
 {
-	stringstream stream;
-	Marker marker = markers.at(i);
+    if (i < 0 || i >= markers.size()) {
+        return "Invalid marker index!";
+    }
 
-	stream << marker.type << " - ";
-	stream << "(" << marker.startPosition.x;
-	stream << ", " << marker.startPosition.y;
-	stream << ", " << marker.startPosition.z << ")";
+    std::stringstream stream;
+    Marker marker = markers.at(i);
 
-	return stream.str();
+    stream << marker.type << " - ";
+    stream << "(" << marker.startPosition.x;
+    stream << ", " << marker.startPosition.y;
+    stream << ", " << marker.startPosition.z << ")";
+
+    return stream.str();
 }
 
 int MarkerManager::numMarkers()
 {
-	return markers.size();
+    return markers.size();
 }
-
 
 bool MarkerManager::addMarker(Marker marker)
 {
-	return false;
+    markers.push_back(marker);
+    return true;  // Successfully added the marker
 }
-
 
 bool MarkerManager::removeMarker()
 {
-	return false;
+    if (!markers.empty()) {
+        markers.pop_back();
+        return true;  // Successfully removed the last marker
+    }
+    return false;  // No markers to remove
 }
-
